48a05a8bf40ad0ac87541b49688a19032fc9084a
Felipe Neaime <fneaime@gmail.com>
2022-09-19 20:53:35 -0300

    Service: Create method to send message using topic str-topic

---
+++ src/main/java/com/felipe/strproducer/services/StringProduceService.java
@@ -0,0 +1,19 @@
+package com.felipe.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StringProduceService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message);
+    }
+
+}
